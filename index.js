9992dac597ef044547c0c4828bf9f4f30f53dcae
Digambaran <arjunsreekumar.1@gmail.com>
2022-06-14 09:33:00 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+const f_create_graph = async (req, res) => {
+
+  // health check
+  if (req.params["health"] === "health") {
+    res.write(JSON.stringify({success: true, msg: "Health check success"}))
+    res.end()
+  }
+
+  // Add your code here
+  res.write(JSON.stringify({success: true, msg: `Hello f_create_graph`}))
+  res.end()
+  
+}
+
+export default f_create_graph
